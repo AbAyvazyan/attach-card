1130f5c966f952d55b127a4b9927efc19b485e6e
AbAyvazyan333 <abayvazyan333@gmail.com>
2024-05-03 00:08:57 +0400

    resolved

--- src/components/FormWrapper/index.tsx
+++ src/components/FormWrapper/index.tsx
@@ -2,7 +2,6 @@ import ExchangeCalculator from "@components/ExchangeCalculator";
 import AttachedCards from "@components/AttachedCards";
 import Card from "@components/Card";
 import CheckboxPart from "@components/CheckboxPart";
-<<<<<<< HEAD
 import {onSubmitHandler} from "@utils/onSubmitHandler";
 
 import styles from './style.module.scss'
@@ -22,22 +21,6 @@ const FormWrapper = () => {
             </div>
         </form>
     )
-=======
-
-import styles from './style.module.scss'
-
-const FormWrapper = () => {
-    return <form className={styles.form}>
-        <div className={styles.formContent}>
-            <h1>Пополнить банковской картой</h1>
-            <ExchangeCalculator/>
-            <AttachedCards/>
-            <Card/>
-            <CheckboxPart/>
-            <button className={styles.submitButton}>Оплатить</button>
-        </div>
-    </form>
->>>>>>> 6de41de95923dcc746c9bfab1a6a8b3dc9dfbef4
 }
 
 export default FormWrapper
